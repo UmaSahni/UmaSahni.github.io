8f3315650e0c50edbd3771ea6f1d23fb676bf27b
Uma Sahni <monkeytown64@gmail.com>
2023-03-27 20:12:18 +0530

    Src Added

--- src/Components/GitHub/Git.jsx
+++ src/Components/GitHub/Git.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactMarkdown from 'react-markdown'
 const Git = () => {
   return (
-    <div id="github-stats-card" >
+    <div id="github-stats-card" src="" >
     <ReactMarkdown>
        [![GitHub Streak](https://github-readme-streak-stats.herokuapp.com?user=UmaSahni)](https://git.io/streak-stats)
     </ReactMarkdown>
--- src/Components/GitHub/GitHubStats.jsx
+++ src/Components/GitHub/GitHubStats.jsx
@@ -3,7 +3,7 @@ import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
 
 const GitHubStats = () => {
   return (
-    <div id="github-streak-stats" >
+    <div src="" id="github-streak-stats" >
         <ReactMarkdown>[![Uma GitHub stats](https://github-readme-stats.vercel.app/api?username=UmaSahni)](https://github.com/anuraghazra/github-readme-stats)</ReactMarkdown>
     </div>
   )
