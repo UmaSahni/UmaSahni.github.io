d6330d1e80afb70d7247e77551240bba34c4bc66
Uma Sahni <monkeytown64@gmail.com>
2023-05-10 14:08:19 +0530

    Intro Added

--- src/Pages/About/Intro.jsx
+++ src/Pages/About/Intro.jsx
@@ -5,7 +5,7 @@ const Intro = () => {
   return (
     <div id="about" className="about section" >
       
-           <Heading  fontFamily={"Inter"} mt={"8"} mb={"8"} >About Me</Heading>
+           <Heading  fontFamily={"Inter"} mt={"4"} mb={"4"} >About Me</Heading>
 
       <div className="nav-link about">
           <h1
@@ -15,17 +15,22 @@ const Intro = () => {
           >
            
           </h1>
-          <h3 id="user-detail-name"  > hi   Uma Sahni </h3>
-        
+          <Heading id="user-detail-name"  > Hi &#x1F44B; I'm  Uma Sahni </Heading>
+         <br/>
+          <Container maxW='container.lg' >
           <Text id="user-detail-intro"   size={"2"}  style={{ letterSpacing: "1px", }}>
             An enthusiastic Mern Developer with excellent hands-on experience in
             developing scalable websites and application using a wide range of
-            frontend and backend skills like HTML, CSS, JavaScript, React,
-            Redux and Node Js. Highly skilled in design, development and
+            frontend and backend skills like <span fontWeight={"bold"} > HTML, CSS, JavaScript, React,
+            Redux and Node Js. </span> Highly skilled in design, development and
             implementation of functional specifications.
-
           </Text>
-          
+          <br/>
+<Text>
+ Apart from coding I am also highly interested in creating illustrations and animations. I use Adobe illustrator to create illustrations. I have also sold some of my creativity to my clients on fiverr with 5 stars rating. I also have some understanding on creating animations. I use Adobe Character Animator and Adobe After Effects  to create animation which I use to publish on my YouTube Channel. 
+</Text>
+
+          </Container>
           <br/>
           <button  id="resume-button-2" style={{ marginTop: "5px" }}>
             <a
--- src/Pages/UmaAbout.jsx
+++ src/Pages/UmaAbout.jsx
@@ -11,7 +11,7 @@ import Intro from "./About/Intro";
 
 const UmaAbout = () => {
   return (
-    <div  style={{ width: "85%", margin: "auto" }}>
+    <div    style={{ width: "85%", margin: "auto",backgroundColor:"white", borderRadius:"1rem", padding:"2rem, 0", marginTop:"3rem" }}>
       <Intro/>
     </div>
   );
